b31b014ab27f57d8b829c5dd8fc63b80e99b7dae
Isaaks12 <isaakscr12@gmail.com>
2022-11-08 11:59:01 -0500

    Avance 8nov

---
+++ index.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/slider1.jpg">
+	</section>
+	<section class="titulo">
+		<h2>Adopción</h2>
+	</section>
+	
+	<section class="adopcion1">
+		<img src="images/cardog.jpg">
+		<section class="cardog">
+			<h3>FIRULAIS</h3>
+			<p>
+				El perro es un animal fascinante, y desde hace más de 40 mil años que fue domesticado, (aunque hay expertos que datan la domesticación de este animal hace más de 100 mil años), desde ese momento el Canis lupus familiaris, más conocido como perro, no ha dejado de sorprendernos gracias a su innegable lealtad, gran valentía, pero, sobre todo, por su enorme habilidad para amarnos como especie humana
+			</p>
+		</section>
+	</section>
+	<section class="adopcion2">
+		<img src="images/carcat.jpg">
+		<section class="carcat">
+			<h3>MOJI</h3>
+			<p>
+				El gato doméstico, llamado más comúnmente gato, y de forma coloquial minino, michino, michi, micho, mizo,  miz,  morroñoo morrongo, y algunos nombres más, es un mamífero carnívoro de la familia Felidae. Es una subespecie domesticada, por la convivencia con el ser humano
+			</p>
+		</section>
+	</section>
+	<section class="vTitle">
+		<h2>Veterinarios</h2>
+	</section>
+	<section class= "vets">
+		<section class="vet1">
+		<img src="images/vet1.jpg">
+		<section class="carVet1">
+			<h3>Adolf Hernandez</h3>
+			<p>
+				Adolf Hernandez, catedrático del Departamento de Sanidad Animal de la Facultad de Veterinaria de la Universidad Complutense de Madrid y coordinador del grupo de investigación SALUVET (Salud Veterinaria y Zoonosis)
+			</p>
+		</section>
+	</section>
+		<section class="vet2">
+		<img src="images/vet2.jpg">
+		<section class="carVet2">
+			<h3>Cristina Galvez</h3>
+			<p>
+				Probablemente Cristi Galvez sea una de las veterinarias más famosas del mundo, y es que son su programa Cristinini ha llegado a todas las casas del mundo para contarnos extraordinarias historias que suceden en el hospital veterinario más sofisticado de Fairbanks, Alaska.
+			</p>
+		</section>
+	</section>
+		<section class="vet3">
+		<img src="images/vet3.jpg">
+		<section class="carVet3">
+			<h3>Jefferson Young</h3>
+			<p>
+				Otro amante de los animales que nos ha transmitido su pasión y amor por estos a todos los espectadores es el médico veterinario Jeff Young que nos ha ayudado a conocer casos fascinantes del mundo veterinario
+			</p>
+		</section>
+	</section>
+	</section>
+		
+	</section>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i>
+			<a href=""><i class="fa-brands fa-instagram"></i>
+			<a href=""><i class="fa-brands fa-tiktok"></i>
+	</footer>
+</div>
+</body>
+</html>
